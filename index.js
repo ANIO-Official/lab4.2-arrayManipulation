9d674bed6f14d961ab9ee43ef618d72f251d5edb
Amanda Ogletree <amandaogletree19@gmail.com>
2025-10-27 16:43:22 -0400

    Started Task 2. Added filterItems function with paramter for query. Tested if function worked using global variable. Check cleared.

--- index.js
+++ index.js
@@ -3,27 +3,57 @@
 //Global Variables
 
 let shoppingList = []
-
+let query = "hat"
 
 //----------------------------------------------
-//Task 1 | 
+//Task 1 | Array Manipulation Basics
 
-//Add an item to the array using push
-function addItem(item){
-    shoppingList.push(item)
-}
+//Add an item to the array using push | Commented due to update task in Task 2
+// function addItem(item){
+//     shoppingList.push(item)
+// }
 function removeLastItem(){
     shoppingList.pop()
 }
 function displayList(){
     for (let listItem of shoppingList){
-        console.log(listItem) // to check if it was added. Testing purposes
+        console.log(`current item: ${listItem}`) // to check if it was added. Testing purposes
     }
 }
-// displayList()
-// addItem("sneakers")
-// addItem("gloves")
-// addItem("tater tots")
-// displayList()
-// removeLastItem()
-// displayList()
+
+//Task 2 | Filter and Search an Array
+function addItem(item){
+    shoppingList.push(item)
+}
+addItem("gloves")
+addItem("t-shirt")
+addItem("Gloves")
+addItem("pink gloves")
+displayList()
+
+//For Loop to check for matches
+
+function filterItems(query){
+    //Go through entire array and search for a match for global Variable query
+    function checkList(){
+        for (let i = 0; i < shoppingList.length; i++){
+            if(shoppingList[i] === query){
+                console.log(`You already added ${shoppingList[i]}`)
+                return true
+            }
+            else { 
+                console.log("This item does not match.")
+                return false}
+        }
+    }
+    let result = shoppingList.filter((checkList))
+}
+filterItems(query)
+
+// function addItem(item){
+//     if(filterItems(shoppingList)){
+
+//     }
+//     else{shoppingList.push(item)} 
+// }
+
