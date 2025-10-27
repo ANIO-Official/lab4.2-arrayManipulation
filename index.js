47060b3b7ff4a7c6013f7cb04eb3c107bae90c7e
Amanda Ogletree <amandaogletree19@gmail.com>
2025-10-27 15:10:27 -0400

    Added blank HTML file, Added index.js file with Task 1 started. Added README file.

---
+++ index.js
@@ -0,0 +1,22 @@
+//Lab 4.2 | Array Manipulation
+
+//Global Variables
+
+let shoppingList = []
+
+
+//----------------------------------------------
+//Task 1 | 
+
+//Add an item to the array using push
+function addItem(item){
+    shoppingList.push(item)
+}
+function removeLastItem(){
+    shoppingList.pop()
+}
+function displayList(){
+    for (let listItem of shoppingList){
+        console.log(listItem) // to check if it was added. Testing purposes
+    }
+}
